43bb914e32441b5072d9ff4e739c630bba2b61cf
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2022-01-23 06:02:17 -0600

    Add RoboFriends React App

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+//import LashesJeniferApp from "./Apps/LashesJeniferApp";
+import RoboFriendsApp from "./Apps/RoboFriendsApp";
+import "tachyons"
+
+const App = () =>{
+    return(
+        <div>
+        <RoboFriendsApp />
+        </div>
+    );
+}
+
+export default App;
