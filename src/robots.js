2f59cd236e904cd1938db30ebbe83a905c9c7218
Jesus Antonio Tiburcio Mejia1 <tiburcio@ibm.com>
2022-01-23 06:06:38 -0600

    creating robots json similar to API

---
+++ src/robots.js
@@ -0,0 +1,62 @@
+export const robots = [
+    {
+        id: 1,
+        name: 'Leanne Graham',
+        username: "Bret",
+        email: "Sincere@april.biz"
+    },
+    {
+        id: 2,
+        name: 'Ervin Howell',
+        username: "Antonette",
+        email: "Shanna@nelissa.tv"
+    },
+    {
+        id: 3,
+        name: 'Clementine Bauch',
+        username: "Samantha",
+        email: "Nathan@yesenia.net"
+    },
+    {
+        id: 4,
+        name: 'Patricia Lebsack',
+        username: "Karianne",
+        email: "Julianne.Oconner@kory.org"
+    },
+    {
+        id: 5,
+        name: 'Leanne Graham',
+        username: "Bret",
+        email: "Sincere@april.biz"
+    },
+    {
+        id: 6,
+        name: 'Leanne Graham',
+        username: "Bret",
+        email: "Sincere@april.biz"
+    },
+    {
+        id: 7,
+        name: 'Leanne Graham',
+        username: "Bret",
+        email: "Sincere@april.biz"
+    },
+    {
+        id: 8,
+        name: 'Leanne Graham',
+        username: "Bret",
+        email: "Sincere@april.biz"
+    },
+    {
+        id: 9,
+        name: 'Glenna Reichert',
+        username: "Delphine",
+        email: "Chain_McDernott@dana.io"
+    },
+    {
+        id: 10,
+        name: 'Clementina DuBuque',
+        username: "Moriah.Stanton",
+        email: "Rey.Padberg@karina.biz"
+    },
+]
